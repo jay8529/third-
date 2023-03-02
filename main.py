2f7128a06f582094607c506d6f2eca5865a85eb7
jay8529 <jayantvijayjondhale@gmail.com>
2023-03-02 20:02:37 +0530

    this is wrong

---
+++ main.py
@@ -0,0 +1,6 @@
+lis = []
+for i in range (1, 21):
+    if i%2 ==0:
+        lis.append(i)
+
+print(lis)
---
+++ reverse.py
@@ -0,0 +1,12 @@
+j = "language"
+
+reverse = " "
+for i in j:
+    reverse = j + reverse
+
+print(reverse)
+
+a = 58
+b = 33
+c = a + b
+print(c)
